08d201743dc311c9fb7676d73df1c3223437497d
zalachenka <saliou.armony@gmail.com>
2019-08-27 16:09:49 +0200

    ternaires done

---
+++ ternaires.js
@@ -0,0 +1,4 @@
+const age = prompt("How old are you?");
+age >= 18 ? alert("Full price: 30€") : alert("Half price: 15€");
+const genre = prompt("What are you ? Enter MALE or FEMALE");
+genre === "MALE" ? alert("35€") : genre === "FEMALE" ? alert("30€") : alert("Try again");
